97aee58edc1b59fd4d33a6a322d8c20689533b6e
Lukas Wojnicki <lukasz.m.wojnicki@gmail.com>
2023-07-13 22:15:16 +0100

    chore(plugins.zsh): add asdf.plugin.zsh to the list of loaded plugins for enhanced functionality chore(plugins.zsh): remove rtx version manager installation and related configurations since its causing errors

--- zsh/.config/zshrc.d/plugins.zsh
+++ zsh/.config/zshrc.d/plugins.zsh
@@ -22,6 +22,7 @@ zinit light starship/starship
 
 # Plugins
 zinit load Aloxaf/fzf-tab
+zinit load alanjjenkins/asdf.plugin.zsh
 zinit load alanjjenkins/kube-aliases
 zinit load fabiokiatkowski/exercism.plugin.zsh
 zinit load joepvd/zsh-hints
@@ -30,20 +31,35 @@ zinit load molovo/tipz
 zinit load zsh-users/zsh-completions
 
 # Install rtx version manager (replacement for asdf)
-zinit ice from"gh-r" as"command" mv"rtx* -> rtx" \
-  atclone'./rtx complete -s zsh > _rtx' atpull'%atclone'
-zinit light jdxcode/rtx
-eval "$(rtx activate zsh)"
+# zinit ice from"gh-r" as"command" mv"rtx* -> rtx" \
+#   atclone'./rtx complete -s zsh > _rtx' atpull'%atclone'
+# zinit light jdxcode/rtx
+# eval "$(rtx activate zsh)"
+#
+# # install zoxide
+# rtx global zoxide@0.9.0 &>/dev/null
+# zinit light ajeetdsouza/zoxide
+#
+# # Setup direnv
+# eval "$(rtx exec direnv -- direnv hook zsh)"
+#
+# # A shortcut for asdf managed direnv.
+# direnv() { rtx exec direnv -- direnv "$@"; }
 
-# install zoxide
-rtx global zoxide@0.9.0 &>/dev/null
-zinit light ajeetdsouza/zoxide
+if ! command -v zoxide &>/dev/null; then
+        if command -v &>/dev/null; then
+                cargo install zoxide
+        fi
+fi
+
+eval "$(zoxide init zsh)"
 
 # Setup direnv
-eval "$(rtx exec direnv -- direnv hook zsh)"
+eval "$(asdf exec direnv hook zsh)"
 
 # A shortcut for asdf managed direnv.
-direnv() { rtx exec direnv -- direnv "$@"; }
+direnv() { asdf exec direnv "$@"; }
+
 
 zinit snippet 'https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/plugins/aws/aws.plugin.zsh'
 zinit snippet 'https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/plugins/command-not-found/command-not-found.plugin.zsh'
@@ -75,6 +91,15 @@ case $(uname) in
     esac
   ;;
 esac
+
+# Sort completion out
+# zinit wait lucid for \
+ # atinit"ZINIT[COMPINIT_OPTS]=-C; zicompinit; zicdreplay" \
+    # zdharma-continuum/fast-syntax-highlighting \
+ # blockf \
+    # zsh-users/zsh-completions \
+# atload"!_zsh_autosuggest_start" \
+    # zsh-users/zsh-autosuggestions
 
 zinit ice lucid wait"0a" from"gh-r" as"program" atload'eval "$(mcfly init zsh)"' bpick"${mcfly_os}"
 zinit light cantino/mcfly
